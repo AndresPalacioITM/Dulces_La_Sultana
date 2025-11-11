4744e3d743236eb7b9d4367f09f5c6ecfa28dfdb
Sara Ospina <saraospi2022@gmail.com>
2025-11-11 00:30:36 -0500

    Entidades con validaciones y relaciones

--- Sultana.Shared/Entities/ProductoTerminado.cs
+++ Sultana.Shared/Entities/ProductoTerminado.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Sultana.Shared.Entities
 {
+
     public class ProductoTerminado
     {
-        public int IdPT { get; set; }
+        public int Id { get; set; }
+
+        [Display(Name = "Producto")]
+        [Required, MaxLength(120)]
         public string Nombre { get; set; } = null!;
+
+        [JsonIgnore] public ICollection<OrdenProduccion> Ordenes { get; set; } = new List<OrdenProduccion>();
     }
 }
