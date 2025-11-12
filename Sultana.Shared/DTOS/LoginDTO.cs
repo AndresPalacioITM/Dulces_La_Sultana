c94f5fe6c5a6daf45abf2ba32d335fa0a2bfa6bf
juanjon vega <juanjosevegaescobar@outlook.es>
2025-11-11 20:02:26 -0500

    correccion para DTOs

--- Sultana.Shared/DTOS/LoginDTO.cs
+++ Sultana.Shared/DTOS/LoginDTO.cs
@@ -5,17 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sultana.Shared.DTOS
-{
-    public class LoginDTO
-    {
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        [EmailAddress(ErrorMessage = "Debes ingresar un correo válido.")]
-        public string Email { get; set; } = null!;
+namespace Sultana.Shared.DTOs;
 
-        [Display(Name = "Contraseña")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        [MinLength(6, ErrorMessage = "El campo {0} debe tener al menos {1} carácteres.")]
-        public string Password { get; set; } = null!;
-    }
+public class LoginDTO
+{
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    [EmailAddress(ErrorMessage = "Debes ingresar un correo válido.")]
+    public string Email { get; set; } = null!;
+
+    [Display(Name = "Contraseña")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    [MinLength(6, ErrorMessage = "El campo {0} debe tener al menos {1} carácteres.")]
+    public string Password { get; set; } = null!;
 }
