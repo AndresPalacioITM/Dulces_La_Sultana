ded3080eed39201a76c631ee6ce3cd9896bc17c8
AndresPalacioITM <andrespalacio307239@correo.itm.edu.co>
2025-11-25 20:46:20 -0500

    Configuracion de Register

---
+++ Sultana.API/Migrations/20251126014304_AddEmailProveedores.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sultana.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailProveedores : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Proveedores",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Proveedores");
+        }
+    }
+}
