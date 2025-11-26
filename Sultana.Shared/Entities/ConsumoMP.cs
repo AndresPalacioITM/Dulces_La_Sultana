3afb3cd933d8dcd63ff29ebbcc4614bcb1eb32aa
Santiago Quintero <santiagoxox09@gmail.com>
2025-11-25 23:29:16 -0500

    Arreglo de formularios

--- Sultana.Shared/Entities/ConsumoMP.cs
+++ Sultana.Shared/Entities/ConsumoMP.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Sultana.Shared.Entities;
@@ -28,6 +29,7 @@ public class ConsumoMP
 
     //public OrdenProduccion OrdenProduccion { get; set; } = null!;
     //public LoteMateriaPrima LoteMateriaPrima { get; set; } = null!;
+    [JsonIgnore]
     public Empleado Responsable { get; set; } = null!;
 
     
